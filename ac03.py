4fe0285e6ab4d477d68c104ee6387608c5d3f208
Gil Xavier <gilxaavier1@gmail.com>
2022-10-05 19:49:43 -0300

    ultimo commit

--- ac03.py
+++ ac03.py
@@ -6,95 +6,93 @@ class Pessoa:
 
 class Professor (Pessoa):
     def __init__(self, nome, nascimento):
-        super().__init__(nome, nascimento)   
+        super().__init__(nome, nascimento)
         self.disciplinas = []
-         
+
     def incluir_disciplina(self, disciplina):
         self.disciplinas.append(disciplina)
-        
+
 
 class Aluno (Pessoa):
     def __init__(self, nome, nascimento, tipo):
         super().__init__(nome, nascimento)
-        self.tipo = tipo 
+        self.tipo = tipo
         self.casa = None
         self.__triunfos = 0
         self.__mau_feitos = 0
         self.disciplinas = []
-        
+
     def incluir_disciplina(self, disciplina):
         self.disciplinas.append(disciplina)
-    
+
     def incluir_triunfo(self, quantidade):
         self.__triunfos += quantidade
-    
+
     def incluir_mau_feito(self, quantidade):
         self.__mau_feitos += quantidade
-    
-    def get_triunfos (self):
+
+    def get_triunfos(self):
         return self.__triunfos
-    
+
     def get_mau_feitos(self):
         return self.__mau_feitos
-    
-    def set_casa (self, Casa):
-        self.casa = Casa
-    
 
-class Disciplina :
+    def set_casa(self, Casa):
+        self.casa = Casa
+
+
+class Disciplina:
     def __init__(self, nome,  ementa):
         self.nome = nome
-        self.ementa = ementa 
-    
-    
+        self.ementa = ementa
+
+
 class Casa:
     def __init__(self, nome, animal):
         self.nome = nome
-        self.animal = animal 
+        self.animal = animal
         self.__diretor = None
         self.__monitor = None
-    
+
     def get_diretor(self):
         return self.__diretor
-    
+
     def get_monitor(self):
         return self.__monitor
-    
-    def set_diretor (self, diretor):
+
+    def set_diretor(self, diretor):
         self.__diretor = diretor
-        
-    def set_monitor (self, monitor):
+
+    def set_monitor(self, monitor):
         self.__monitor = monitor
-    
+
 
 class Escola:
     def __init__(self, nome):
-        self.nome = nome 
+        self.nome = nome
         self.casas = []
-        
-    def incluir_casa (self, casa):
+
+    def incluir_casa(self, casa):
         self.casas.append(casa)
-        
+
 
 class Torneio:
     def __init__(self, casa1, casa2):
         self.casa1 = casa1
-        self.casa2 = casa2 
+        self.casa2 = casa2
         self.__pontos_casa1 = 0
         self.__pontos_casa2 = 0
-    
+
     def marcar_ponto(self, casa, quantidade):
         if casa == self.casa1:
- 
+
             self.__pontos_casa1 += quantidade
         else:
-   
+
             self.__pontos_casa2 += quantidade
-            
-        
-        
-    def get_pontos_casa1 (self):
+
+    def get_pontos_casa1(self):
         return self.__pontos_casa1
-    
-    def get_pontos_casa2 (self):
-        return self.__pontos_casa2    
+
+    def get_pontos_casa2(self):
+        return self.__pontos_casa2
